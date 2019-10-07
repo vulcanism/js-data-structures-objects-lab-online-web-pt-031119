82a6ffc478b5395170b12ca113b5ce0ad79502e4
Savannah C <sgcorrell@gmail.com>
2019-10-07 15:55:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 const driver = {};
+
+function updateDriverWithKeyAndValue(obj, key, value) {
+  const newDriver = {...obj};
+  newDriver[key] = value;
+  return newDriver;  
+}
+
+function destructivelyUpdateDriverWithKeyAndValue() {
+  
+}
+
+function deleteFromDriverByKey() {
+  
+}
+
+function destructivelyDeleteFromDriverByKey() {
+  
+}
